19db64eb0d691063747ec436f63d911a84b5b4a1
Michal Vancura <lomivan88@gmail.com>
2023-01-06 18:03:45 +0100

    Add date to output informations

--- restart_server.py
+++ restart_server.py
@@ -3,17 +3,19 @@ import re
 with open("data.txt", "r", encoding="utf8") as f:
     text = f.readlines()
     line_before = ""
+    
     for line in text:
         if re.search("syslogd", line) and re.search("restart", line):
             if re.search("exiting", line_before):
-                server_stop = line_before.rsplit()[2]
-                print(f"{server_stop}: Server turned off")
-                server_start = line.rsplit()[2]
-                print(f"{server_start}: Server restarted")
+                server_stop = line_before.rsplit()[0:3]
+                print(f"{' '.join(server_stop)}: Server turned off")
+                server_start = line.rsplit()[0:3]
+                print(f"{' '.join(server_start)}: Server restarted")
             else:
                 last_info = line_before.rsplit()[2]
-                print(f"{last_info}: Last information! Server crashed !")
+                print(f"{' '.join(last_info)}: Last information! Server crashed !")
                 server_start = line.rsplit()[2]
-                print(f"{server_start}: Server restarted")          
+                print(f"{' '.join(server_start)}: Server restarted")      
+                    
         line_before = line
             
