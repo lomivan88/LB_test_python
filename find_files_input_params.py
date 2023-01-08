73990957a62d7482fcfe8286a09bdaf9e6944756
Michal Vancura <lomivan88@gmail.com>
2023-01-08 18:00:29 +0100

    Search also with mask by fnmatch library

--- find_files_input_params.py
+++ find_files_input_params.py
@@ -1,7 +1,8 @@
 import os
 import sys
+import fnmatch
 
-file_to_find = ""
+file_to_find = "" 
 path = ""
 recursive = ""
 
@@ -15,25 +16,35 @@ except IndexError as e:
 if path == "":    
     try: 
         path = sys.argv[2]
+        try:
+            recursive = sys.argv[3]
+        except IndexError as e:
+            pass               
     except IndexError as e:
         print("Path is missing!")
         path = input("Path where to search the file: ")
         recursive = input("Recursive searching (type 'R' if yes): ").upper()
     
 
-def find_file_recursive(filename, path):
-    print("Found files:")
-    for dirpath, folders, files in os.walk(path):
-        for file in files:
-            if file == file_to_find:
+def find_file_recursive(filename, folder_path):
+    if os.path.exists(folder_path): 
+        print("Matched files in specified folder and subfolders:")
+        for dirpath, folders, files in os.walk(folder_path):
+            founded_files = fnmatch.filter(files, file_to_find)
+            for file in founded_files:
                 print(os.path.join(dirpath, file))
+    else:
+        print(f"Wrong path! {e}")
                 
-def find_file(filename, path):
-    print("Found files:")
-    files = [item for item in os.listdir(path) if os.path.isfile(f"{path}\{item}")]
-    for f in files:
-        if f == file_to_find:
-            print(f"{path}\{f}")
+def find_file(filename, folder_path):
+    try:
+        print("Matched files in specified folder:")
+        files = [item for item in os.listdir(folder_path) if os.path.isfile(f"{folder_path}\{item}")]
+        founded_files = fnmatch.filter(files, file_to_find)
+        for f in founded_files:
+            print(f"{folder_path}\{f}")
+    except OSError as e:
+        print(f"Wrong path! {e}")
         
 if __name__ == "__main__":
     if recursive == "R":
