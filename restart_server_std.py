a1f41508b51fe3f44aae1e53814db8309d84603a
Michal Vancura <lomivan88@gmail.com>
2023-01-09 19:44:33 +0100

    Delete comments

--- restart_server_std.py
+++ restart_server_std.py
@@ -8,12 +8,8 @@ for line in sys.stdin:
         if re.search("exiting", line_before):
             s_stop = re.match(regex_date_time, line)
             s_start = re.match(regex_date_time, line_before)
-            # server_stop = re.split(r"\s+", line)[0:3]
-            # server_start = re.split(r"\s+",line_before)[0:3]
             sys.stdout.write(f"{s_stop.group(0)}: Server turned off\n{(s_start.group(0))}: Server restarted")
         else:
-            # last_info = line_before.rsplit()[0:3]
-            # server_start = line.rsplit()[0:3]
             l_info = re.match(regex_date_time, line_before)
             s_start = re.match(regex_date_time, line_before)
             sys.stdout.write(f"{l_info.group(0)}: Server crashed! \n{s_start.group(0)}: Server restarted")
